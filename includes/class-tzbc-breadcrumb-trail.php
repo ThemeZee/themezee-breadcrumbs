e122433090bdb4f5956624e14ff6ccb37e61b57e
Thomas W <netzberufler@googlemail.com>
2016-05-05 10:22:10 +0200

    fixed wrong order of child categories

--- includes/class-tzbc-breadcrumb-trail.php
+++ includes/class-tzbc-breadcrumb-trail.php
@@ -1090,7 +1090,7 @@ class TZBC_Breadcrumb_Trail {
 
 		// If we have parent terms, reverse the array to put them in the proper order for the trail.
 		if ( !empty( $parents ) )
-			$this->items = array_merge( $this->items, $parents );
+			$this->items = array_merge( $this->items, array_reverse( $parents ) );
 	}
 
 	/**
--- themezee-breadcrumbs.php
+++ themezee-breadcrumbs.php
@@ -5,7 +5,7 @@ Plugin URI: https://themezee.com/plugins/breadcrumbs/
 Description: This plugin allows you to add a nice and elegant breadcrumb navigation. Breadcrumbs make it easy for the user to navigate up and down the hierarchy of your website and are good for SEO.
 Author: ThemeZee
 Author URI: https://themezee.com/
-Version: 1.0.2
+Version: 1.0.3
 Text Domain: themezee-breadcrumbs
 Domain Path: /languages/
 License: GPL v3
@@ -66,7 +66,7 @@ class ThemeZee_Breadcrumbs {
 		define( 'TZBC_NAME', 'ThemeZee Breadcrumbs' );
 
 		// Define Version Number
-		define( 'TZBC_VERSION', '1.0.2' );
+		define( 'TZBC_VERSION', '1.0.3' );
 		
 		// Define Plugin Name
 		define( 'TZBC_PRODUCT_ID', 49729 );
